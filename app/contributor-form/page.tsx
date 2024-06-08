9059ceebf1754dfc87dacec0df910b924de5792b
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2024-06-08 01:25:30 +0100

    form submission

--- app/contributor-form/page.tsx
+++ app/contributor-form/page.tsx
@@ -2,11 +2,12 @@
 // import react from "React";
 import AllFormFields from '@/components/forms/page';
 import formStyles from './forms.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 
-
-export default function UsersPage(){   
+const UsersPage = () => {   
 
     return(
         
@@ -16,8 +17,8 @@ export default function UsersPage(){
 
             {/* Form Section */}
 
-            <div className={formStyles.mainFormContainer}>
-                <div>
+            <div className={formStyles.mainFormContainer} >
+                <div >
                     <h1> Add Project</h1>
 
                     <div style={{}}>
@@ -25,7 +26,32 @@ export default function UsersPage(){
                         {/* All Form Fields */}
 
                         <AllFormFields/>
-                        
+                        <div className={formStyles.socialFooter}>
+                            <div className={formStyles.socialicons}>
+                                <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/facebook.png" width="30" height="30" alt="Facebook icon" />
+                                </Link>
+                                <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/instagram.png" width="30" height="30" alt="Instagram icon" />
+                                </Link>
+                                <Link href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/twitter.png" width="30" height="30" alt="Twitter icon" />
+                                </Link>
+                                <Link href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                                    <Image src="/linkedin.png" width="30" height="30" alt="LinkedIn icon" />
+                                </Link>
+                            </div>
+
+                            <div className={formStyles.socialLinks}>
+                                <div>Terms of service</div>
+                                <div>Privacy policy</div>
+                                <div>Contact us</div>
+                            </div>
+
+                            <div className={formStyles.socialRights}>
+                            © 2024 Transition School, Inc. All rights reserved.
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -33,4 +59,4 @@ export default function UsersPage(){
     )
 }
 
-// export default UsersPage;
+export default UsersPage;
