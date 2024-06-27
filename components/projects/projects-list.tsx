91aa3633211a07fe860a79add05faaaaa7d2bc0b
Yomi Omotoso <yomi@shopwired.uk>
2024-06-27 09:37:58 +0100

    bug fixes

--- components/projects/projects-list.tsx
+++ components/projects/projects-list.tsx
@@ -2,7 +2,7 @@ import ProjectCard from "./project-card";
 import iProject from "./project.interface";
 import heroStyles from "../../app/components/landingPage.module.css"
 
-export default function ProjectsList({ projects, columns, gap = 8 }: { projects: iProject[], columns: number, gap: number }) {
+export default function ProjectsList({ projects, columns, gap = 4 }: { projects: iProject[], columns: number, gap?: number }) {
     const gridColumnsClass = columns === 4 ? "grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3" : " grid-cols-2 sm:grid-cols-2 gap-3 md:grid-cols-3 md:gap-7 ";
     const gridPaddingClass = columns === 4 ? "px-0 md:px-9" : "";
     const gapClass = columns === 4 && gap === 8 ? "gap-4 lg:gap-8" : "";
