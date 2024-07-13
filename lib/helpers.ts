ed303a887260d19a4e8476afe5b57f6e114886b3
Yomi Omotoso <yomi@shopwired.uk>
2024-07-13 11:54:06 +0100

    project image

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,3 +1,3 @@
 export function getUploadImage(path: string) {
-    return "https://ts-labs-admin-0ff0c6162225.herokuapp.com/uploads" + path;
+    return "https://ts-labs-admin-0ff0c6162225.herokuapp.com" + path;
 }
